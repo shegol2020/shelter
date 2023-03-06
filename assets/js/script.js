c64ef3666937ec00b13f724cbd8ec644766e9cf3
shegol2020 <shegol2007@gmail.com>
2023-03-06 15:23:01 +0300

    feat: burger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 //burger
 
 let hamMenu = document.querySelector(".hamburger-lines");
+let mobMenu = document.querySelector(".mobile-menu");
 hamMenu.addEventListener("click", () =>{
     hamMenu.classList.toggle("active");
+    mobMenu.classList.toggle("mobile-menu-active");
 });
