bf1c6fcc232fc68387e330bb5a413178f2400fe2
shegol2020 <shegol2007@gmail.com>
2023-03-07 19:33:12 +0300

    feat: burger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
 //burger
 
-let hamMenu = document.querySelector(".hamburger-lines");
+let burgerIcon = document.querySelector(".hamburger-lines");
 let mobMenu = document.querySelector(".mobile-menu");
-hamMenu.addEventListener("click", () =>{
-    hamMenu.classList.toggle("active");
+let bodyEl = document.body;
+
+burgerIcon.addEventListener("click", () =>{
+    burgerIcon.classList.toggle("active");
     mobMenu.classList.toggle("mobile-menu-active");
+    bodyEl.classList.toggle("stop-scrolling");
+    //add shadow
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target.classList.contains('header-nav-item-link')) {
+        burgerIcon.classList.toggle("active");
+        mobMenu.classList.toggle("mobile-menu-active");
+        bodyEl.classList.toggle("stop-scrolling");
+    }
 });
