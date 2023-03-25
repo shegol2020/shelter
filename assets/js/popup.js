6a6c1477ef7f18a321665a39edb5d11d8513efb7
shegol2020 <shegol2007@gmail.com>
2023-03-25 07:49:46 +0300

    feat: pop-up-style

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -14,40 +14,34 @@ function popUpRender(event) {
     console.log(petsArray);
     const popUpCard = document.querySelector('.pop-up-content');
 
-    petsArray[petNum].id;
-
     popUpCard.innerHTML =  `<button class="pop-up-close">X</button>
-        <img src="../../assets/img/jennifer.png" alt="" class="pop-up-img">
+<div class="pop-up-card">
+    <div class="pop-up-img-wrap">
+        <img src="../../assets/img/jennifer.png" alt="" class="pop-up-img"></div>
         <div class="pop-up-info">
-            <h2 class="pop-up-title">${petsArray[petNum].name}</h2>
-            <h3 class="pop-up-subtitle">Dog - Labrador</h3>
-            <p class="pop-up-main-info">Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.</p>
-            <ul class="pop-up-list">
-                <li class="pop-up-list-item">Age: 2 months</li>
-                <li class="pop-up-list-item">Age: 2 months</li>
-                <li class="pop-up-list-item">Age: 2 months</li>
-                <li class="pop-up-list-item">Age: 2 months</li>
-            </ul>
-        </div>`
+            <div class="pop-up-header">
+                <h2 class="pop-up-title heading-medium">${petsArray[petNum].name}</h2>
+                <h3 class="pop-up-subtitle heading-small">Dog - Labrador</h3></div>
+                <p class="pop-up-main-info heading-extra-small">Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.</p>
+                <ul class="pop-up-list">
+                    <li class="pop-up-list-item">Age: 2 months</li>
+                    <li class="pop-up-list-item">Age: 2 months</li>
+                    <li class="pop-up-list-item">Age: 2 months</li>
+                    <li class="pop-up-list-item">Age: 2 months</li>
+                </ul>
+            </div>
+        </div>
+      </div>
+ </div>`
 
+    const popUpCloseBtn = document.querySelector('.pop-up-close');
 
+    popUpCloseBtn.addEventListener('click', () => {
+        popUpElement.classList.add('hidden');
+        document.body.classList.remove('stop-scrolling');
 
+    });
 
-
-
-
-
-
-
-
-    // const popUpCloseBtn = document.querySelector('.pop-up-close');
-    //
-    // popUpCloseBtn.addEventListener('click', () => {
-    //     popUpElement.classList.add('hidden');
-    //     document.body.classList.remove('stop-scrolling');
-
-    // });
-
-    // document.body.classList.add('stop-scrolling');
+    document.body.classList.add('stop-scrolling');
 
 }
